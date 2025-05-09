0ec846fc8c477eaae1cccee0c89e2469a4c678a6
Michael Gross <mgstinger521@gmail.com>
2025-05-08 17:12:12 -0700

    Add functionality to disable Windows Copilot in debloat script

--- win11_debloat.ps1
+++ win11_debloat.ps1
@@ -28,6 +28,11 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Siuf\Rules" -Name "NumberOfSIUF
 # Kill Cortana
 Get-AppxPackage -Name Microsoft.549981C3F5F10 | Remove-AppxPackage -AllUsers
 
+# Kill Copilot
+# Note: Windows Copilot is a new feature in Windows 11, and its registry keys may change in future updates.
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsCopilot" -Force | Out-Null
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsCopilot" -Name "TurnOffWindowsCopilot" -Type DWord -Value 1
+
 # Disable background apps
 Get-AppxPackage | ForEach-Object {
     $packageName = $_.Name
@@ -37,3 +42,6 @@ Get-AppxPackage | ForEach-Object {
 # Disable Start Menu ads and suggestions
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SystemPaneSuggestionsEnabled" -Value 0
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SubscribedContent-338389Enabled" -Value 0
+
+# Restart Explorer to apply changes
+Stop-Process -Name explorer -Force
