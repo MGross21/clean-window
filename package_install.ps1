9e802466c98dd9ae9f8db3947c5ec997aa42d2b2
Michael Gross <mgstinger521@gmail.com>
2025-05-09 15:30:21 -0700

    Refactor package installation to read package list from external configuration file

--- package_install.ps1
+++ package_install.ps1
@@ -1,12 +1,12 @@
-$apps = @(
-    "Notepad++.Notepad++",
-    "Microsoft.VisualStudioCode",
-    "7zip.7zip",
-    "VideoLAN.VLC",
-    "Valve.Steam",
-    "OBSProject.OBSStudio",
-    "Discord.Discord"
-)
+# Read package list from external configuration file
+$packageFile = "./packages.conf"
+if (Test-Path $packageFile) {
+    $apps = Get-Content $packageFile
+} else {
+    Write-Error "Package configuration file not found: $packageFile"
+    exit 1
+}
+
 foreach ($app in $apps) {
     winget install $app --accept-package-agreements --accept-source-agreements -h
 }
